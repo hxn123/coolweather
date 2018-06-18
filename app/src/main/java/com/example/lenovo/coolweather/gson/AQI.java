88f22c6e4ef504c0e9662e21e8bba134a07878d8
hxn123 <2365813876@qq.com>
2018-06-18 13:48:53 +0800

    最后提交

---
+++ app/src/main/java/com/example/lenovo/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.coolweather.gson;
+
+/**
+ * Created by lenovo on 2018/6/16.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/coolweather/gson/Basic.java
@@ -0,0 +1,20 @@
+package com.example.lenovo.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by lenovo on 2018/6/16.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+
+}
